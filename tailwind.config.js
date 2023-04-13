8a25a4df5403bf6c1a126dd7e48294efede9b020
Luis <looes1@gmail.com>
2023-04-13 18:24:44 +1000

    init commit portfolio v0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'my-red': '#ff0000',
+      'my-blue': '#0000ff',
+      'my-green': '#00ff00',
+      'my-gray': '#666666',
+      'my-pink': '#ff69b4',
+    }
   },
   plugins: [],
 }
